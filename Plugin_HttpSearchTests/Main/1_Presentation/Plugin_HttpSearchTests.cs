c70b65c18dd04b60fc6ff9eb19590fa4d1204fb2
Ruben Unteregger <ruben.unteregger@gmail.com>
2018-02-25 10:35:09 +0100

    Add tests

---
+++ Plugin_HttpSearchTests/Main/1_Presentation/Plugin_HttpSearchTests.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minary.Plugin.Main.HttpSearch.DataTypes.Class;
+using MinaryLib;
+
+
+namespace Minary.Plugin.Main.Tests
+{
+
+  [TestClass()]
+  public class Plugin_HttpSearchTests 
+  {
+
+    #region MEMBERS
+
+    private PluginProperties properties;
+    private Plugin_HttpSearch inst;
+    private Moq.Mock<MinaryLib.Plugin.IPluginHost> minaryHost;
+
+    #endregion
+
+
+    #region TESTS
+
+    [TestInitialize()]
+    public void Initialize()
+    {
+      this.minaryHost = new Moq.Mock<MinaryLib.Plugin.IPluginHost>();
+      this.properties = new PluginProperties();
+
+      this.properties.HostApplication = (MinaryLib.Plugin.IPluginHost)this.minaryHost.Object;
+      this.properties.ApplicationBaseDir = @"c:\temp\";
+      this.properties.PluginBaseDir = @"c:\temp\";
+
+      this.inst = new Plugin_HttpSearch(properties);      
+    }
+
+
+    [TestMethod()]
+    public void AddDataRecord()
+    {
+      this.inst.OnNewData("Testrecord");
+      Assert.IsTrue(this.inst.DataBatch.Count > 0);
+    }
+
+
+    [TestMethod()]
+    public void ProcessData_NoPatterns()
+    {
+      this.inst.OnNewData("Testrecord");
+      this.inst.ProcessEntries();
+      Assert.IsTrue(this.inst.DataBatch.Count == 0);
+      Assert.IsTrue(this.inst.HttpFindingRedcords.Count == 0);
+    }
+
+
+    [TestMethod()]
+    public void ProcessData_NonMatchingPatterns()
+    {
+      RecordHttpSearch pattern = new RecordHttpSearch("GET", @"invalidhost\.com", "/invalid/path/.*", "invalidDataRegex");
+      this.inst.HttpSearchRecords.Add(pattern);
+      this.inst.OnNewData("Testrecord");
+      this.inst.ProcessEntries();
+
+      Assert.IsTrue(this.inst.DataBatch.Count == 0);
+      Assert.IsTrue(this.inst.HttpSearchRecords.Count == 1);
+      Assert.IsTrue(this.inst.HttpFindingRedcords.Count == 0);
+    }
+
+    
+    [TestMethod()]
+    public void ProcessData_MatchingMethod()
+    {
+      RecordHttpSearch pattern = new RecordHttpSearch("GET", @".*", "/.*", ".*");
+      this.inst.AddRecord(pattern);
+      this.inst.OnNewData("TCP||11-22-33-44-55-66||192.168.10.10||1234||8.8.8.8||80||" +
+                          "GET /index.html HTTP/1.1..Host: host.com....<html><body>hello world</body></html>");
+      this.inst.ProcessEntries();
+      
+      Assert.IsTrue(this.inst.HttpSearchRecords.Count == 1);
+      Assert.IsTrue(this.inst.HttpFindingRedcords.Count == 1);
+    }
+    
+
+    [TestMethod()]
+    public void ProcessData_MatchingHost()
+    {
+      RecordHttpSearch pattern = new RecordHttpSearch("GET", @"intranet\.host\.com", "/.*", ".*");
+      this.inst.AddRecord(pattern);
+      this.inst.OnNewData("TCP||11-22-33-44-55-66||192.168.10.10||1234||8.8.8.8||80||" +
+                          "GET /index.html HTTP/1.1..Host: intranet.host.com....<html><body>hello world</body></html>");
+      this.inst.ProcessEntries();
+
+      Assert.IsTrue(this.inst.HttpSearchRecords.Count == 1);
+      Assert.IsTrue(this.inst.HttpFindingRedcords.Count == 1);
+    }
+
+
+    [TestMethod()]
+    public void ProcessData_MatchingPath()
+    {
+      RecordHttpSearch pattern = new RecordHttpSearch("GET", ".*", @"/subdir/index\.html", ".*");
+      this.inst.AddRecord(pattern);
+      this.inst.OnNewData("TCP||11-22-33-44-55-66||192.168.10.10||1234||8.8.8.8||80||" +
+                          "GET /subdir/index.html HTTP/1.1..Host: host.com....<html><body>hello world</body></html>");
+      this.inst.ProcessEntries();
+
+      Assert.IsTrue(this.inst.HttpSearchRecords.Count == 1);
+      Assert.IsTrue(this.inst.HttpFindingRedcords.Count == 1);
+    }
+
+
+    [TestMethod()]
+    public void ProcessData_MatchingData()
+    {
+      RecordHttpSearch pattern = new RecordHttpSearch("GET", ".*", ".*", "world");
+      this.inst.AddRecord(pattern);
+      this.inst.OnNewData("TCP||11-22-33-44-55-66||192.168.10.10||1234||8.8.8.8||80||" +
+                          "GET /subdir/index.html HTTP/1.1..Host: host.com....<html><body>hello world</body></html>");
+      this.inst.ProcessEntries();
+
+      Assert.IsTrue(this.inst.HttpSearchRecords.Count == 1);
+      Assert.IsTrue(this.inst.HttpFindingRedcords.Count == 1);
+    }
+
+
+    [TestMethod()]
+    public void ProcessData_SearchUsernameAndPassword()
+    {
+      RecordHttpSearch pattern = new RecordHttpSearch("POST", ".*", ".*", @"user=(\w+)\b.*&pass=(\w)");
+      this.inst.AddRecord(pattern);
+      this.inst.OnNewData("TCP||11-22-33-44-55-66||192.168.10.10||1234||8.8.8.8||80||" +
+                          "POST /login.php HTTP/1.1..Host: host.com....param1=someValue&user=joos&param2=anotherValue&pass=randomletters&param3=blah");
+      this.inst.ProcessEntries();
+
+      Assert.IsTrue(this.inst.HttpSearchRecords.Count == 1);
+      Assert.IsTrue(this.inst.HttpFindingRedcords.Count == 1);
+    }
+
+
+    [TestMethod()]
+    public void ProcessData_InvalidDataPattern()
+    {
+      RecordHttpSearch pattern = new RecordHttpSearch("GET", ".*", ".*", @"[world");
+      Assert.ThrowsException<System.Exception>(() => this.inst.AddRecord(pattern));
+    }
+
+    #endregion
+
+  }
+}
+
---
+++ Plugin_HttpSearchTests/Main/1_Presentation/ShowRequestTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.RegularExpressions;
+
+
+namespace Minary.Plugin.Main.Tests
+{
+  [TestClass()]
+  public class ShowRequestTests
+  {
+
+    #region TESTS
+    
+    [TestMethod()]
+    public void ShowRequestTest()
+    {
+      var httpData = "POST /login.php HTTP/1.1..Host: host.com....param1=someValue&user=joos&param2=anotherValue&pass=randomletters&param3=blah";
+      var hostRegex = ".*";
+      var pathRegex = ".*";
+      var dataRegex = @"user=(\w+)\b.*&pass=(\w+)";
+
+      ShowRequest inst = new ShowRequest(httpData, hostRegex, pathRegex, dataRegex);
+      string rtfOutput = inst.GenerateRtf(httpData, dataRegex);
+      
+      Assert.IsTrue(Regex.Match(rtfOutput, @"user=\s*\\b\s*joos\s*\\b\s*").Success == true);
+      Assert.IsTrue(Regex.Match(rtfOutput, @"pass=\s*\\b\s*randomletters\s*\\b\s*").Success == true);
+    }
+
+    #endregion
+
+  }
+}
